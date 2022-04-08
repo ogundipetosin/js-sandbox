355e3f216a915bcfac899299bf02314ca5f0ff96
ogundipe oluwatosin <tosynchic@gmail.com>
2022-04-08 21:06:03 +0100

    arrayandobject

---
+++ arrayandobject.js
@@ -0,0 +1,28 @@
+var facebook={
+    username:"tosin",
+    password:"123"
+  }
+  console.log(facebook);
+  //second assignment
+  var database=[
+  {
+    facebook
+  }
+  ]
+  console.log(database);
+  //third assignment
+  var newsfeed=[
+    {
+      username:"tosin",
+      timeline:"123"
+    },
+    {
+      username:"tosin",
+      timeline:"123"
+    },
+    {
+      username:"tosin",
+      timeline:"123"
+    }
+  ]
+  console.log(newsfeed[2].timeline);
